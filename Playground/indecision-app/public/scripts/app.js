53d695f3a51ff3726135f5b7888a998ea4062e98
something <nnebp@users.noreply.github.com>
2018-07-27 09:18:08 -0400

    react working

--- Playground/indecision-app/public/scripts/app.js
+++ Playground/indecision-app/public/scripts/app.js
@@ -1,1 +1,13 @@
 console.log('running!');
+
+
+//JSX
+//var template = <p>JSX text test</p>;
+var template = React.createElement(
+  "h1",
+  { id: "someid" },
+  "Something new"
+);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
