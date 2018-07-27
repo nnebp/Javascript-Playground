19c462e34536dd2faa8afec8e07dae6dbb966876
something <nnebp@users.noreply.github.com>
2018-07-27 14:59:53 -0400

    challenge 2

--- Playground/indecision-app/public/scripts/app.js
+++ Playground/indecision-app/public/scripts/app.js
@@ -1,12 +1,43 @@
+'use strict';
+
 console.log('running!');
+//babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 
 
 //JSX
-//var template = <p>JSX text test</p>;
+var title = {
+  title: 'look im a title',
+  subTitle: 'subtitle over here'
+};
+
 var template = React.createElement(
-  "h1",
-  { id: "someid" },
-  "Something new"
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    title.title
+  ),
+  React.createElement(
+    'h2',
+    null,
+    title.subTitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    'age:26'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'location:26sss'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'JSX text isssssssss7777777777sst'
+  )
 );
 
 var appRoot = document.getElementById('app');
