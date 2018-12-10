d38c2e4883d78a3073697be0f7d3eeab76f2d0f5
something <nnebp@users.noreply.github.com>
2018-12-10 17:21:54 -0500

    echo test working

--- Playground/KadenceTest/test.js
+++ Playground/KadenceTest/test.js
@@ -42,14 +42,14 @@ node.listen(listenPort);
 // Join a known peer by it's [identity, contact]
 //if (joinNode !== undefined) { //TODO we still have to join?
 if (true) { //TODO we still have to join?
-  node.join([joinID, { //is this running by default??
+  node.join([joinID, { // does this even matter on the same machine?
 
     hostname: joinIP,
     port: joinPort
   }], () => {
     // Add 'join' callback which indicates peers were discovered and
     // our node is now connected to the overlay network
-    node.logger.info(`Connected to ${node.router.size} peers!`)
+    node.logger.info(`Connected to ${node.router.size} peers!`);
     console.log(identity.valueOf());
 
     // Base protocol exposes:
@@ -58,5 +58,23 @@ if (true) { //TODO we still have to join?
     // * node.iterativeStore(key, value, callback)
   });
 }
+
+// Use "userland" (that's you!) rules to create your own protocols
+node.use('ECHO', (request, response, next) => {
+/*  if ([/!* some naughty words *!/].includes(request.params.message)) {
+    return next(new Error(
+      `Oh goodness, I dare not say "${request.params.message}"`
+    ));
+  }*/
+  console.log(request.params.message + " <-- did you also see this one?");
+
+  //response.send(request.params);
+});
+
+const neighbor = [
+  ...node.router.getClosestContactsToKey(node.identity).entries(),
+].shift();
+
+node.send('ECHO', { message: "will i see this?" }, neighbor);
 
 
