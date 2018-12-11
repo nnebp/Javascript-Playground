fa2c2bba607c668ec6979f0b72d1f18afe2790c7
something <nnebp@users.noreply.github.com>
2018-12-10 22:55:07 -0500

    flood working

--- Playground/KadenceTest/test.js
+++ Playground/KadenceTest/test.js
@@ -10,6 +10,7 @@ const levelup = require('levelup');
 const leveldown = require('leveldown');
 const encoding = require('encoding-down');
 const kadence = require('@kadenceproject/kadence');
+const uuid = require('uuid/v4');
 
 // Command line args
 const listenPort = process.argv[2];
@@ -17,6 +18,8 @@ const joinID = process.argv[3];
 const joinIP = process.argv[4];
 const joinPort= process.argv[5];
 //TODO if argv.length < blah blah
+
+const receivedFloodMessages = [];
 
 // In production, persist identity to disk and load it
 // Generating a new one every time will cause lookup problems
@@ -40,41 +43,45 @@ const node = new kadence.KademliaNode({
 node.listen(listenPort);
 
 // Join a known peer by it's [identity, contact]
-//if (joinNode !== undefined) { //TODO we still have to join?
-if (true) { //TODO we still have to join?
-  node.join([joinID, { // does this even matter on the same machine?
+node.join([joinID, { // does this even matter on the same machine?
 
-    hostname: joinIP,
-    port: joinPort
-  }], () => {
-    // Add 'join' callback which indicates peers were discovered and
-    // our node is now connected to the overlay network
-    node.logger.info(`Connected to ${node.router.size} peers!`);
-    console.log(identity.valueOf());
+  hostname: joinIP,
+  port: joinPort
+}], () => {
+  // Add 'join' callback which indicates peers were discovered and
+  // our node is now connected to the overlay network
+  node.logger.info(`Connected to ${node.router.size} peers!`);
+  console.log(identity.valueOf());
 
-    // Base protocol exposes:
-    // * node.iterativeFindNode(key, callback)
-    // * node.iterativeFindValue(key, callback)
-    // * node.iterativeStore(key, value, callback)
-  });
-}
+  // Base protocol exposes:
+  // * node.iterativeFindNode(key, callback)
+  // * node.iterativeFindValue(key, callback)
+  // * node.iterativeStore(key, value, callback)
 
-// Use "userland" (that's you!) rules to create your own protocols
-node.use('ECHO', (request, response, next) => {
-/*  if ([/!* some naughty words *!/].includes(request.params.message)) {
-    return next(new Error(
-      `Oh goodness, I dare not say "${request.params.message}"`
-    ));
-  }*/
-  console.log(request.params.message + " <-- did you also see this one?");
-
-  //response.send(request.params);
 });
 
-const neighbor = [
-  ...node.router.getClosestContactsToKey(node.identity).entries(),
-].shift();
+// Use "userland" (that's you!) rules to create your own protocols
+node.use('FLOOD', (request, response, next) => {
+  if (receivedFloodMessages.indexOf(request.params.id) === -1) {
+    //TODO send the message on
+    console.log(request.params.message + " <-- did you also see this one?");
 
-node.send('ECHO', { message: "will i see this?" }, neighbor);
+    flood(request.params.message, request.params.id);
+    receivedFloodMessages.push(request.params.id);
+  }
+});
 
+//send out a flood
+const flood = (message, id) => {
+  for(let value of node.router.values()) {
+    if (value.size > 0) {
+      for(let identValue of value) {
+        //console.log(identValue);
+        //TODO send real message
+        node.send('FLOOD', {message: message, id: id}, identValue);
+      }
+    }
+ }
+};
 
+flood("333333dd333333", uuid());
